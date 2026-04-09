2aa4a140f094e6ec3ebb5628377b9f8c4d10a96c
Thuy Le <ca.happy95@gmail.com>
2026-04-08 18:16:58 -0700

    update api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { AdminStudent, AdminStudentCreateInput, AdminTeacher, AdminTeacherCreate
 import { StoredUser } from '@/utils/authStub';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = process.env.API_BASE_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
